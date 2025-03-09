5864e0779b8b719288d83f4f272a8b5e1452a340
st10343093 <st10343093@vcconnect.edu.za>
2025-03-09 21:51:07 +0200

    Add project files.

---
+++ icetask1/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using icetask1.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace icetask1.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        //constructor that passes the DbContext options to the base class for configuration
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        //DbSet representing the collection of ToDoTask entities in the database
+        public DbSet<ToDoTask> Tasks { get; set; }
+    }
+}
